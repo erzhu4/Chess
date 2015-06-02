4482d6c845b8b0d1c649498d08877fca568c0522
Eric and Charles <eric+charles@appacademy.io>
2015-06-02 12:21:02 -0400

    Implement sliding chess pieces.

--- piece.rb
+++ piece.rb
@@ -1,5 +1,7 @@
 class Piece
 
+  attr_reader :color
+
   def initialize(board, pos, color)
     @board = board
     @pos = pos
@@ -65,3 +67,82 @@ class SteppingPiece < Piece
   end
 
 end#######################################
+
+class Pawn < Piece
+
+  def initialize(board, pos, color)
+    super(board, pos, color)
+    @dir = (@color == :white) ? [1, 0] : [-1, 0]
+    @sides = [[0, 1], [0, -1]]
+  end
+
+  def valid_moves
+    x, y = @pos
+
+    dx, dy = @dir
+    move = [x + dx, y + dy]
+    return [] unless Board.valid_pos?(move)
+
+    moves = []
+    moves << move unless @board[move]
+
+    @sides.each do |sx, sy|
+      side_move = [x + dx + sx, y + dy + sy]
+      if @board[side_move] && @board[side_move].color != @color
+        moves << side_move
+      end
+    end
+
+
+    moves
+  end
+end#####################################################################
+
+class Rook < SlidingPiece
+
+  DIRS = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+  ]
+
+  def initialize(board, pos, color)
+    super(board, pos, color, DIRS)
+  end
+
+end
+
+class Bishop < SlidingPiece
+
+  DIRS = [
+    [1, 1],
+    [-1, 1],
+    [1, -1],
+    [-1, -1]
+  ]
+
+  def initialize(board, pos, color)
+    super(board, pos, color, DIRS)
+  end
+
+end
+
+class Queen < SlidingPiece
+
+  DIRS = [
+    [1, 1],
+    [-1, 1],
+    [1, -1],
+    [-1, -1],
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1]
+  ]
+
+  def initialize(board, pos, color)
+    super(board, pos, color, DIRS)
+  end
+
+end
